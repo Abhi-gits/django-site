492aaf492dc09ef8301e8166e0afcf5e2d160648
Abhishek Gupta <ravi37463@gmail.com>
2023-05-17 17:25:12 +0530

    error-checked

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('djangosite.urls')),
+    path('api/', include('djangosite.urls_api')),
     
 ]
